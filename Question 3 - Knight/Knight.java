8f438e552c2db1b5317e756d59381b80976ff6d0
jabij1 <joey.abihashim@gmail.com>
2011-06-06 21:15:12 -0700

    Edited Question 3 - Knight/Knight.java via GitHub

--- Question 3 - Knight/Knight.java
+++ Question 3 - Knight/Knight.java
@@ -7,10 +7,10 @@ public class Knight {
          length = 8;
          width = 8;
          board = new int[width][length];
-         setBoard();
+         set();
     }
 
-    private void setBoard() {
+    private void set() {
         for(int i=0;i<length;i++) {
             for(int j=0;j<width;j++) {
                 board[i][j]=0;
@@ -18,7 +18,7 @@ public class Knight {
         }
     }
     
-    public boolean place(int x, int y, int moves) {
+    public boolean position(int x, int y, int moves) {
         if (x < 0 || x >= width || y < 0 || y >= length) {
             return false;
         } else if (board[y][x] > 0) {
@@ -30,7 +30,7 @@ public class Knight {
             System.out.printf("Position: (%d,%d,%d)%n", x,y, moves);
             board[y][x] = moves;
             moves++;  
-            if (place(x-2, y-1, moves) || place(x-2, y+1,  moves) || place(x-1, y+2,  moves)|| place(x+1, y+2, moves) || place(x+2, y+1, moves) || place(x+2, y-1,  moves) || place(x+1, y-2,  moves) || place(x-1, y-2,  moves)) {
+            if (position(x-2, y-1, moves) || position(x-2, y+1,  moves) || position(x-1, y+2,  moves)|| position(x+1, y+2, moves) || position(x+2, y+1, moves) || position(x+2, y-1,  moves) || position(x+1, y-2,  moves) || position(x-1, y-2,  moves)) {
                 return true;
             } else {
                 board[y][x] = 0;
